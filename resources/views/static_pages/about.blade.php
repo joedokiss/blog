037c6e930b7cfba8160b3bf62ffd6d0b12f83b7d
vagrant <vagrant@homestead>
2019-06-18 11:04:54 +0000

    basic static pages and default layout

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', 'Title - About')
+
+@section('content')
+<h1>About page</h1>
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('title', 'Title - Home')
+
+@section('content')
+<h1>Home page</h1>
+@stop
