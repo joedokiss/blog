e7ffda7545a9be18c15797781260a747b06d19b4
vagrant <vagrant@homestead>
2019-06-22 11:31:20 +0000

    make header and footer as sub view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,18 +6,12 @@
 
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">Help</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">Login</a></li>
-        </ul>
-      </div>
-    </nav>
+    @include('layouts._header')
 
     <div class="container">
       @yield('content')
+
+      @include('layouts._footer')
     </div>
   </body>
 </html>
