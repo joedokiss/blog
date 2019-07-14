70867ab5414b257392cbd1eb494aca22efd6fdfb
vagrant <vagrant@homestead>
2019-07-14 12:19:33 +0000

    changes

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        // returned an array with inputs if validation pass
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+
+        // Auth::attempt(['email' => $email, 'password' => $password])
+        // 接收一个数组来作为第一个参数，该参数提供的值将用于寻找数据库中的用户数据
+        // 第二个参数为是否为用户开启『记住我』功能的布尔值
+        if (Auth::attempt($credentials, $request->has('remember')))
+        {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        }else{
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+            // withInput() works for 'old()' in the view template
+            return redirect()->back()->withInput();
+        }
+
+        return;
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login'); //what is difference from route('login')?
+    }
+}
---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.default')
+@section('title', 'Login')
+
+@section('content')
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>Login</h5>
+    </div>
+    <div class="card-body">
+      @include('shared._errors')
+
+      <form method="POST" action="{{ route('login') }}">
+          {{ csrf_field() }}
+
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">Remember me</label>
+            </div>
+          </div>
+
+          <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+
+      <hr>
+
+      <p>No account?<a href="{{ route('signup') }}">Sign Up now!</a></p>
+    </div>
+  </div>
+</div>
+@stop
