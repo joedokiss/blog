6887d655dcf28f38775457b4d81afaab83f0fff2
vagrant <vagrant@homestead>
2019-08-09 12:12:51 +0000

    statistics information

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,25 +1,24 @@
 @extends('layouts.default')
 
 @section('content')
-  @if(Auth::check())
+  @if (Auth::check())
     <div class="row">
-
       <div class="col-md-8">
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
       </div>
-
-      <h4>微博列表</h4>
-      <hr>
-      @include('shared._feed')
-
       <aside class="col-md-4">
         <section class="user_info">
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
+        <section class="stats mt-2">
+          @include('shared._stats', ['user' => Auth::user()])
+        </section>
       </aside>
-
     </div>
   @else
     <div class="jumbotron">
