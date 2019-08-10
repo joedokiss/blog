dfb12d4524b7aea1589ea62bc2cd8fd8233b14d4
vagrant <vagrant@homestead>
2019-08-10 12:36:22 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,15 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+
+        array_push($user_ids, $this->id);
+
+        return Status::whereIn('user_id', $user_ids)
+                            ->with('user')
+                            ->orderBy('created_at', 'desc');
+
+        // return $this->statuses()->orderBy('created_at', 'desc');
     }
 
     // 方法会在用户模型类完成初始化之后进行加载
